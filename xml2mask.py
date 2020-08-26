871160abf4fb6c3f7f52a6c212556d4709a65ae0
hjoonjang <jjaang2570@naver.com>
2020-08-26 13:10:41 +0900

    Mult 255 before saving for convenient reviews

--- xml2mask.py
+++ xml2mask.py
@@ -1,6 +1,7 @@
 import numpy as np
 import xml.etree.ElementTree as et
 import os, glob, re
+from tqdm import tqdm
 import tifffile, cv2
 import openslide
 
@@ -8,10 +9,12 @@ wsi_load_dir = './wsi_folder/'
 xml_load_dir = './xml_folder/'
 wsi_fns = sorted(glob.glob(wsi_load_dir + '*.svs') + glob.glob(wsi_load_dir + '*.SVS'))
 xml_fns = sorted(glob.glob(xml_load_dir + '*.xml') + glob.glob(xml_load_dir + '*.XML'))
-save_dir = './mask_img_l2/'
-os.makedirs(save_dir, exist_ok=True)
-div = 16                                   ## Level0 scale to Level2 scale
+level = 2
+div = 4**level                             ## Level0 scale to Level2 scale
 assert len(wsi_fns) == len(xml_fns) == 47  ## the number of training_data WSI pool
+
+save_dir = f'./mask_img_l{level}/'
+os.makedirs(save_dir, exist_ok=True)
 
 q = re.compile('training_data_[0-9]{2}')
 
@@ -43,7 +46,7 @@ def xml2mask(xml_fn, shape):
     rs = regions[0].findall('Region')
     plistlist = list()
     nlistlist = list()
-    print('rs:', len(rs))
+    #print('rs:', len(rs))
     for i, r in enumerate(rs):
       ylist = list()
       xlist = list()
@@ -85,10 +88,10 @@ def xml2mask(xml_fn, shape):
   return ret
 
 def save_mask(xml_fn, shape):
-  wsi_id = xml_fn.split('/')[-1][:-4]
-  save_fn = save_dir + '{}_l2_annotation_tumor.tif'.format(wsi_id)
+  wsi_id = q.findall(xml_fn)[0]
+  save_fn = save_dir + f'{wsi_id}_l{level}_annotation_tumor.tif'
   ret = xml2mask(xml_fn, shape)
-  tifffile.imsave(save_fn, (ret[1]>0).astype(np.uint8), compress=9)
+  tifffile.imsave(save_fn, (ret[1]>0).astype(np.uint8)*255, compress=9)
 
 def load_svs_shape(fn, level=2):
   imgh = openslide.OpenSlide(fn)
@@ -96,12 +99,10 @@ def load_svs_shape(fn, level=2):
 
 
 if __name__ == '__main__':
-  for i, (wsi_fn, xml_fn) in enumerate(zip(wsi_fns, xml_fns)):
+  for wsi_fn, xml_fn in tqdm(zip(wsi_fns, xml_fns), total=len(wsi_fn)):
     wsi_id = q.findall(wsi_fn)[0]
     xml_id = q.findall(xml_fn)[0]
     assert wsi_id == xml_id
-    assert os.path.isfile(wsi_fn) and os.path.isfile(xml_fn)
-    print(i+1, '/', len(wsi_fns), ':', wsi_id)
-    shape = load_svs_shape(wsi_fn, level=2)
+    shape = load_svs_shape(wsi_fn, level=level)
     save_mask(xml_fn, shape)
 
